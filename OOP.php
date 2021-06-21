60341381c0cf1a56c12294dbaa288b0be003c9b6
tsuka414 <keita04141537@icloud.com>
2021-06-21 23:41:17 +0900

    OOP

---
+++ OOP.php
@@ -0,0 +1,15 @@
+<?php
+  class Human {
+      private $name;
+      private $birthday;
+      private $gender;
+      private $height;
+      private $weight;
+
+      public function walk() {
+          echo 
+      }
+
+      $human = new Human();
+
+  }
